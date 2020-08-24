fd084e089255e79326d2d09b92de58d1f9701605
NischalKash <55364746+NischalKash@users.noreply.github.com>
2020-08-23 21:05:04 -0400

    Update execute.py

--- Ruby_Code/execute.py
+++ Ruby_Code/execute.py
@@ -3,7 +3,9 @@ import os
 os.system('ruby bug_fix_ruby.rb')
 repo_dir = ""
 repo = Repo(repo_dir,search_parent_directories=True)
-file_list = ['log.txt']
+
+#Please Enter The absolute path of the log.txt file here!
+file_list = ['/log.txt']
 commit_message = "Testing Scenarios Logs"
 repo.index.add(file_list)
 repo.index.commit(commit_message)
