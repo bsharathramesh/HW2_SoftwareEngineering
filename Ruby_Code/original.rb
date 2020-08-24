280f357180d76736b001bfe57186c7a709f35c7f
amoghrd <amoghrd@gmail.com>
2020-08-24 02:08:00 +0000

    Added comments

--- Ruby_Code/original.rb
+++ Ruby_Code/original.rb
@@ -1,16 +1,32 @@
+#Conway's Game of Life
+#Implemented by Nischal Kashyap: nkashya2
+
+#Simulate Game of Life for given number of Generations
 def game_of_life(rows,columns,grid,generations)
     future = grid
     for x in 1..generations
         puts "Generation #{x}"
+        
+        #Loop through each cell
         for i in 0..future.length-1
             for j in 0..future[i].length-1
                 alive_neighbours = 0
+                
+                #Finding number of neighbours for current cell
                 for a in -1..1
                     for b in -1..1
+                        
+                        #Variables for counting Neighbors
                         x = 0
                         y = 0
+                        
+                        #Current number of rows and columns
                         nx = future.length-1
                         ny = future[i].length-1
+
+                        #Corner Cases including cyclic neighbors
+                        
+                        #Top Left Corner
                         if i==0 and j==0
                             if i+a<0 and j+b<0
                                 x=nx
@@ -25,6 +41,8 @@ def game_of_life(rows,columns,grid,generations)
                                 x = i+a
                                 y = j+b
                             end
+
+                        #Top Right Corner
                         elsif i==0 and j==ny
                             if i+a<0 and j+b>ny
                                 x = nx
@@ -39,6 +57,8 @@ def game_of_life(rows,columns,grid,generations)
                                 x = i+a
                                 y = j+b
                             end
+
+                        #Bottom Right Corner
                         elsif i==nx and j==ny
                             if i+a>nx and j+b>ny
                                 x = 0
@@ -53,6 +73,8 @@ def game_of_life(rows,columns,grid,generations)
                                 x = i+a
                                 y = j+b
                             end
+
+                        #Bottom Left Corner
                         elsif i==nx and j==0
                             if i+a>nx and j+b<0
                                 x = 0
@@ -67,15 +89,25 @@ def game_of_life(rows,columns,grid,generations)
                                 x = i+a
                                 y = j+b
                             end
+
+                        #Edge Cases
+
+                        #Top Edge
                         elsif i==0 and i+a<0
                             x = nx
                             y = j+b
+
+                        #Bottom Edge
                         elsif i==nx and i+a>nx
                             x = 0
                             y = j+b
+
+                        #Left Edge
                         elsif j==0 and j+b<0
                             x = i+a
                             y = ny
+
+                        #Right Edge
                         elsif j==ny and j+b>ny
                             x = i+a
                             y = 0
@@ -86,17 +118,28 @@ def game_of_life(rows,columns,grid,generations)
                         alive_neighbours = alive_neighbours + future[x][y]
                     end
                 end
-                alive_neighbours = alive_neighbours-future[i][j]
 
+                #Current cell to be subtracted to avoid duplicates
+                alive_neighbours = alive_neighbours-future[i][j]
+                
+                #Implenting rules of GOF
+                
+                #Lonely Cell dies
                 if future[i][j]==1 and alive_neighbours<2
                     future[i][j] = 0
+
+                #Cell dies due to overcrowding
                 elsif future[i][j]==1 and alive_neighbours>3
                     future[i][j] = 0
+
+                #A new cell is born if there are 3 neighbors
                 elsif future[i][j]==0 and alive_neighbours==3
                     future[i][j] = 1
                 end
             end
         end
+
+        #Display the new generation
         for a in 0..future.length-1
             for b in 0..future[a].length-1
                 if future[a][b]==1
